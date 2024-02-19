7f21e7aeebd8fca919d2d816b5f1fa1f1835153e
mathieuRA <contact@mathieu-raisin.fr>
2024-02-19 16:51:59 +0100

    feat(xo-web/license): ability to rebind licenses

--- packages/xo-web/src/common/xo/index.js
+++ packages/xo-web/src/common/xo/index.js
@@ -3621,6 +3621,8 @@ export const unlockXosan = (licenseId, srId) => _call('xosan.unlock', { licenseI
 
 export const bindLicense = (licenseId, boundObjectId) => _call('xoa.licenses.bind', { licenseId, boundObjectId })
 
+export const rebindItemLicense = () => _call('xoa.licenses.rebindItem')
+
 export const bindXcpngLicense = (licenseId, boundObjectId) =>
   bindLicense(licenseId, boundObjectId)::tap(subscribeXcpngLicenses.forceRefresh)
 
